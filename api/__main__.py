35250a8c4d235d76af131d51ef8ef1fd3b47da35
Wahyusaputra <91831925+Wahyu213@users.noreply.github.com>
2021-12-23 12:05:02 +0700

    Create __main__.py

---
+++ api/__main__.py
@@ -0,0 +1,38 @@
+from api import predict, app
+from api.functions import download_image
+from config import PORT
+import os
+import uvicorn
+
+model = predict.load_model('nsfw_detector/nsfw_model.h5')
+
+
+@app.get("/")
+async def detect_nsfw(url: str):
+    if not url:
+        return {"ERROR": "URL PARAMETER EMPTY"}
+    image = await download_image(url)
+    if not image:
+        return {"ERROR": "IMAGE SIZE TOO LARGE OR INCORRECT URL"}
+    results = predict.classify(model, image)
+    os.remove(image)
+    hentai = results['data']['hentai']
+    sexy = results['data']['sexy']
+    porn = results['data']['porn']
+    drawings = results['data']['drawings']
+    neutral = results['data']['neutral']
+    if neutral >= 25:
+        results['data']['is_nsfw'] = False
+        return results
+    elif (sexy + porn + hentai) >= 70:
+        results['data']['is_nsfw'] = True
+        return results
+    elif drawings >= 40:
+        results['data']['is_nsfw'] = False
+        return results
+    else:
+        results['data']['is_nsfw'] = False
+        return results
+
+if __name__ == "__main__":
+    uvicorn.run("api:app", host="0.0.0.0", port=PORT, log_level="info")
